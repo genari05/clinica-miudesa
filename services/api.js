be9c7d762b6af792cd782131c190d83dfcb15146
genari05 <tgenari791@gmail.com>
2025-11-13 23:59:15 -0300

    vercel teste

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://api-psicanalise.onrender.com/",
+  baseURL: "https://clinica-psicologia-lovat.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
