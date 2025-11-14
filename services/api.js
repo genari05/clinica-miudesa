caab4839e4e1947f159c704d7582c45bcd66d8c9
genari05 <tgenari791@gmail.com>
2025-11-14 00:01:05 -0300

    arrumando link

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://clinica-psicologia-lovat.vercel.app/",
+  baseURL: "https://api-psicanalise.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
